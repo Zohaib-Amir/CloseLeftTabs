19340093dc9082af5b0dc00bbca2e57a144d3e35
Zohaib Amir <zeedev22@gmail.com>
2016-10-12 19:53:33 +0500

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
+  // Gets all tabs using qeury
   chrome.tabs.query({}, function (tabs) {
     for (var i = 0; i < tab.index; i++) {
         chrome.tabs.remove(tabs[i].id);
