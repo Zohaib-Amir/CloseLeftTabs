b028d12b684ae0e89654faea3cae6cac87f4aa01
Zohaib-Amir <zeedev22@gmail.com>
2019-06-14 05:10:01 +0500

    Fixed Bug with Multiple windows Open, Added Context Menu

--- background.js
+++ background.js
@@ -1,8 +1,26 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Gets all tabs using qeury
-  chrome.tabs.query({}, function (tabs) {
-    for (var i = 0; i < tab.index; i++) {
-        chrome.tabs.remove(tabs[i].id);
+
+
+function removeLeftTabs(win){
+  //Remove each tab until the highlighted one is found
+    for(var i = 0; win.tabs[i].highlighted != true; i++){
+      chrome.tabs.remove(win.tabs[i].id);
     }
-});
+}
+
+
+
+function OnContextMenuClicked(info, tab){
+  chrome.windows.get(tab.windowId, {"populate" : true}, removeLeftTabs);
+
+}
+
+
+chrome.contextMenus.create({"title": "Close Left Tabs",
+"onclick": OnContextMenuClicked});
+
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  //Gets current window along with array of tabs
+  chrome.windows.get(tab.windowId, {"populate" : true}, removeLeftTabs);
+  
 });
